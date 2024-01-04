ee968cb89f14db5f6d1d0a24291a70bb14a03519
saidulislam49 <saidulislam49du@gmail.com>
2024-01-04 15:36:40 +0600

    something

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    // $pdo = DB::connection()->getPdo();
-    // $user = $pdo->query('select * from users')->fetchAll();
-
-    $result = DB::select('select * from users where id =? and name=?',[1, 'Mrs. Rosalind Marquardt I']);
-    $result2 = DB::select('select * from users where id =:id',['id'=>1]);
-    // DB::insert('insert into users(name, email, password) values(?, ?, ?)',['inserted name','inserted@gmail.com','123456']);
-    $result3 = DB::table('users')->select()->get();
-    dump($result3);
+    $result = DB::table('users')->select()->get();
+    dump($result);
     return view('welcome');
 });
